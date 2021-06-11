eecc6273393045deae126b2be9f03afd4eff44ca
Emnon <csampaie.5@gmail.com>
2021-06-11 09:14:21 +0200

    ALAP

---
+++ chat-starter/src/db_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyDvw8CRhCRmL6JTnqC5ccpWio7WRf0K5uY',
+  authDomain: 'chat-app-100200.firebaseapp.com',
+  databaseURL: 'https://chat-app-100200.firebaseio.com',
+  projectId: 'chat-app-100200',
+  storageBucket: 'chat-app-100200.appspot.com',
+  messagingSenderId: '836757650426'
+};
+
+export default config;
